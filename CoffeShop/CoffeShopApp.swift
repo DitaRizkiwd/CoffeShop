95f8761d0a8577208fb7aa0ae040b54612e2a503
DitaRizkiwd <rdita201@gmail.com>
2024-04-17 13:08:15 +0700

    Initial Commit

---
+++ CoffeShop/CoffeShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeShopApp.swift
+//  CoffeShop
+//
+//  Created by MACBOOK PRO on 17/04/24.
+//
+
+import SwiftUI
+
+@main
+struct CoffeShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
